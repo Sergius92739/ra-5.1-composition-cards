457b0fa8c9ce04562b78857014dab20d6ac925a8
Sergius92739 <sergem279@gmail.com>
2024-01-09 00:11:56 +0300

    src fixed

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,7 +8,7 @@ export default function Card(props) {
       <div className="card-body">
         <h5 className="card-title">{props.title}</h5>
         <p className="card-text">{props.text}</p>
-        <a href="https://www.yandex.ru/" className="btn btn-primary">Go somewhere</a>
+        <a href="https://www.yandex.ru/" className="btn btn-primary">Перейти куда-нибудь...</a>
       </div>
     </div>
   )
